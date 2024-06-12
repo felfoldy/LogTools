e88643ff13bec01e6563b9ef3aed631c61714643
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2024-06-12 23:58:09 +0200

    Remove LogToolsTests

--- Package.swift
+++ Package.swift
@@ -7,18 +7,9 @@ let package = Package(
     name: "LogTools",
     platforms: [.macOS(.v11), .iOS(.v14), .watchOS(.v7), .tvOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "LogTools",
-            targets: ["LogTools"]),
+        .library(name: "LogTools", targets: ["LogTools"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "LogTools"),
-        .testTarget(
-            name: "LogToolsTests",
-            dependencies: ["LogTools"]),
+        .target(name: "LogTools")
     ]
 )
